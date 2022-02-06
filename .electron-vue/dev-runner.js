af717fd4a17fb9480054a4daf6c8d3e97986e367
Lennart Pegel <github@justlep.net>
2022-02-06 17:15:43 +0100

    updated html-webpack-plugin

--- .electron-vue/dev-runner.js
+++ .electron-vue/dev-runner.js
@@ -42,13 +42,6 @@ function startRenderer() {
             heartbeat: 2500
         });
 
-        compiler.hooks.compilation.tap('compilation', compilation => {
-            compilation.hooks.htmlWebpackPluginAfterEmit.tapAsync('html-webpack-plugin-after-emit', (data, cb) => {
-                hotMiddleware.publish({action: 'reload'});
-                cb()
-            })
-        });
-
         compiler.hooks.done.tap('done', stats => {
             logStats('Renderer', stats)
         });
